e974d76d166f09a0d86999bcf25d22b4c56fd388
Tom Gillam <tpgillam@googlemail.com>
2023-11-04 22:46:36 +0000

    Make it easy to switch between active buffers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,6 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 	end
 }
