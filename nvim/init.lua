48b3b8348cfa528c9f78b02db3a45ab4c3837302
Tom Gillam <tpgillam@googlemail.com>
2023-11-04 22:48:14 +0000

    Consolidate airline configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,18 +15,7 @@ require("lazy").setup(
 -- We have to enable `termguicolors` for certain colorschemes to work correctly
 vim.opt.termguicolors = true
 
--- Choose the colorscheme (overall, and for airline)
-vim.g.airline_powerline_fonts = 1
-vim.g.airline_theme = "bubblegum"
-
-vim.g.airline_left_sep = ''
-vim.g.airline_left_alt_sep = ''
-vim.g.airline_right_sep = ''
-vim.g.airline_right_alt_sep = ''
-vim.g.airline_symbols.branch = ''
-vim.g.airline_symbols.readonly = ''
-vim.g.airline_symbols.linenr = ''
-
+-- Choose the colorscheme
 
 vim.cmd.colorscheme("jellybeans-nvim")
 
--- nvim/lua/plugins/airline.lua
+++ nvim/lua/plugins/airline.lua
@@ -1,4 +1,34 @@
 return {
-    { "vim-airline/vim-airline" },
+    {
+        "vim-airline/vim-airline",
+        config = function()
+            vim.g.airline_powerline_fonts = 1
+
+            vim.g.airline_left_sep = ''
+            vim.g.airline_left_alt_sep = ''
+            vim.g.airline_right_sep = ''
+            vim.g.airline_right_alt_sep = ''
+
+            -- For some reason, just overriding individual elements of this
+            -- dictionary didn't work. Rather it seems necessary to define it
+            -- from scratch.
+            -- NOTE if we get runtime errors about missing elements from this dictionary,
+            --  it may be necessary to define them.
+            vim.g.airline_symbols = {
+                branch = '',
+                colnr = ' ℅:',
+                readonly = '',
+                linenr = ' :',
+                maxlinenr = '☰ ',
+                dirty = '⚡',
+                space = ' ',
+                notexists = 'Ɇ',
+                whitespace = 'Ξ',
+                keymap = 'Layout:',  -- Unused, but this is the default
+            }
+
+            vim.g.airline_theme = "bubblegum"
+        end
+    },
     { "vim-airline/vim-airline-themes" },
 }
