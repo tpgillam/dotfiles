48a739d66ce1660595bd8a720b2df7cc31ad26b9
Tom Gillam <tpgillam@googlemail.com>
2023-11-04 22:11:51 +0000

    Add airline to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,16 +4,41 @@ vim.g.mapleader = " "
 
 -- Packages should be initialised _after_ remapping the <leader>
 require("lazy").setup(
-	"plugins",
+	"plugins",  -- This will load all files in the `plugins` module.
 	{
+        -- Make it easier to see the edge of the Lazy plugin manager.
 		ui = { border = "double" }
 	}
 )
 
--- Choose the colorscheme
+-- Options
 -- We have to enable `termguicolors` for certain colorschemes to work correctly
 vim.opt.termguicolors = true
-vim.cmd.colorscheme("duskfox")
+
+-- Choose the colorscheme (overall, and for airline)
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_theme = "bubblegum"
+
+vim.g.airline_left_sep = ''
+vim.g.airline_left_alt_sep = ''
+vim.g.airline_right_sep = ''
+vim.g.airline_right_alt_sep = ''
+vim.g.airline_symbols.branch = ''
+vim.g.airline_symbols.readonly = ''
+vim.g.airline_symbols.linenr = ''
+
+
+vim.cmd.colorscheme("jellybeans-nvim")
+
+-- No tabs, please
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Line numbers
+vim.opt.number = true
+
 
 -- Keymappings
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)  -- Show explorer
