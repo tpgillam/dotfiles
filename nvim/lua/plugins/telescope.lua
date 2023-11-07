51ccedabe58edf9bd46adbea2113afabd6518941
Tom Gillam <tpgillam@googlemail.com>
2023-11-07 00:13:14 +0000

    Switch theme

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,10 +6,11 @@ return {
 		require('telescope').load_extension('fzf')
 
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>th', builtin.colorscheme, {})
+        local theme = require("telescope.themes").get_ivy()
+		vim.keymap.set('n', '<leader>pf', function() builtin.find_files(theme) end, {})
+		vim.keymap.set('n', '<leader>pg', function() builtin.live_grep(theme) end, {})
+        vim.keymap.set('n', '<leader>pb', function() builtin.buffers(theme) end, {})
+		vim.keymap.set('n', '<C-p>', function() builtin.git_files(theme) end, {})
+        vim.keymap.set('n', '<leader>th', function() builtin.colorscheme(theme) end, {})
 	end
 }
