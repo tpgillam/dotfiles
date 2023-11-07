10df1fd63a2658d96e625719c134fd795a84769f
Tom Gillam <tpgillam@googlemail.com>
2023-11-07 21:19:21 +0000

    Consolidate theme plugins

---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,15 @@
+-- All plugins which are just colorschemes.
+return {
+    {
+        "rockyzhang24/arctic.nvim",
+        branch = "v2",
+        dependencies = { "rktjmp/lush.nvim" },
+    },
+    {
+        "metalelf0/jellybeans-nvim",
+        dependencies = { "rktjmp/lush.nvim" },
+    },
+    { "savq/melange-nvim" },
+    { "EdenEast/nightfox.nvim" },
+    { "romainl/Apprentice" },
+}
