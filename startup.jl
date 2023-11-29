3b8725a182bc5391e1a2f7f665c15b359ebfa153
Tom Gillam <tpgillam@googlemail.com>
2023-11-29 16:26:02 +0000

    Use OhMyREPL

--- startup.jl
+++ startup.jl
@@ -2,9 +2,13 @@ import Pkg
 
 let
     # Ensure that all of these packages are installed, and are `using`ed.
-    packages = ("Revise",)
+    packages = ("Revise", "OhMyREPL")
     for package in packages
         isnothing(Base.find_package(package)) && Pkg.add(package)
         @eval using $(Symbol(package))
     end
+
+    # Don't use rainbow brackets in OhMyREPL, and don't autcomplete.
+    OhMyREPL.enable_pass!("RainbowBrackets", false)
+    OhMyREPL.enable_autocomplete_brackets(false)
 end
