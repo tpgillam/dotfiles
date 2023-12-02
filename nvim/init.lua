18e7690413b2af6051748cce46a9e6c29a5bc875
Tom Gillam <tpgillam@googlemail.com>
2023-12-02 01:24:32 +0000

    Move keyboard shortcuts for LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,4 +38,11 @@ vim.opt.relativenumber = false
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)  -- Show explorer
 vim.keymap.set("n", "<leader>l", vim.cmd.Lazy)  -- Show Lazy manager
 
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)  -- See more about diagnostics
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)  -- Autoformat with LSP
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)  -- Go to definition
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)  -- Go to implementation
+-- NOTE: There are other LSP actions that we might want to map, e.g.
+--  - lsp.buf.signature_help
+--  - lsp.buf.rename
+--  - ...
