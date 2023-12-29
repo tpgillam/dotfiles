05acb01fdc987fbdc4c98bb2868f73df3fe3fda2
Tom Gillam <tpgillam@googlemail.com>
2023-12-29 12:06:09 +0000

    Bind LSP hover action

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,8 +40,10 @@ vim.keymap.set("n", "<leader>l", vim.cmd.Lazy)              -- Show Lazy manager
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float) -- See more about diagnostics
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)        -- Autoformat with LSP
+vim.keymap.set("n", "gh", vim.lsp.buf.hover)                -- LSP hover action
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)           -- Go to definition
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)       -- Go to implementation
+
 -- NOTE: There are other LSP actions that we might want to map, e.g.
 --  - lsp.buf.signature_help
 --  - lsp.buf.rename
