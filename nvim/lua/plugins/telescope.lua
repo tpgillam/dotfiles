30414ebede788a0712202979369e255f11911487
Tom Gillam <tpgillam@googlemail.com>
2023-12-31 11:43:46 +0000

    Show hidden files in picker

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,16 +1,28 @@
 return {
-	"nvim-telescope/telescope.nvim", branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function ()
-		-- Use fzf-native with telescope
-		require('telescope').load_extension('fzf')
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        -- Use fzf-native with telescope
+        local telescope = require("telescope")
+        telescope.load_extension('fzf')
 
-		local builtin = require('telescope.builtin')
+        local builtin = require('telescope.builtin')
         local theme = require("telescope.themes").get_ivy()
-		vim.keymap.set('n', '<leader>pf', function() builtin.find_files(theme) end, {})
-		vim.keymap.set('n', '<leader>pg', function() builtin.live_grep(theme) end, {})
+        vim.keymap.set('n', '<leader>pf', function() builtin.find_files(theme) end, {})
+        vim.keymap.set('n', '<leader>pg', function() builtin.live_grep(theme) end, {})
         vim.keymap.set('n', '<leader>pb', function() builtin.buffers(theme) end, {})
-		vim.keymap.set('n', '<C-p>', function() builtin.git_files(theme) end, {})
+        vim.keymap.set('n', '<C-p>', function() builtin.git_files(theme) end, {})
         vim.keymap.set('n', '<leader>th', function() builtin.colorscheme(theme) end, {})
-	end
+
+        -- Enable showing hidden files (dotfiles & those in .gitignore) in file finder.
+        telescope.setup {
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = true
+                }
+            }
+        }
+    end
 }
