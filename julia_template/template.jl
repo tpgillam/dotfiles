b2a78709d0c0cafc3ca2b427727ce3f8797e9c1d
Tom Gillam <tpgillam@googlemail.com>
2024-01-01 21:04:28 +0000

    Improve Julia package template

--- julia_template/template.jl
+++ julia_template/template.jl
@@ -1,6 +1,6 @@
 using PkgTemplates
 
-# TODO All of this should really be replaced by something central in PkgTemplates.jl
+# TODO: All of this should really be replaced by something central in PkgTemplates.jl
 
 # Apparently we can only ever use ONE of any particular plugin. This is most vexing.
 
@@ -16,13 +16,7 @@ end
 # TODO: This would be nicer if templated on the CI plugin type, but then I can't use
 # @plugin, and I'm lazy.
 @plugin struct JuliaFormatterGitHubActions <: Plugin
-    style::String = "blue"
-    julia_formatter_toml::String = default_file("JuliaFormatter.toml")
     julia_formatter_yml::String = default_file("julia_formatter.yml")
-end
-
-function PkgTemplates.view(p::JuliaFormatterGitHubActions, t::Template, pkg::AbstractString)
-    return Dict("FORMAT_STYLE" => p.style)
 end
 
 function PkgTemplates.hook(p::JuliaFormatterGitHubActions, t::Template, pkg_dir::AbstractString)
@@ -30,11 +24,6 @@ function PkgTemplates.hook(p::JuliaFormatterGitHubActions, t::Template, pkg_dir:
     outdir = joinpath(pkg_dir, ".github", "workflows")
     mkpath(outdir)
     cp(p.julia_formatter_yml, joinpath(outdir, "julia_formatter.yml"))
-
-    # Generate files.
-    pkg = basename(pkg_dir)
-    formatter_toml = render_file(p.julia_formatter_toml, combined_view(p, t, pkg), tags(p))
-    gen_file(joinpath(pkg_dir, ".JuliaFormatter.toml"), formatter_toml)
 end
 
 
@@ -58,12 +47,14 @@ function tom_template(; dir::Union{Nothing,String}=nothing, julia=v"1.6")
         authors="Tom Gillam <tpgillam@googlemail.com>",
         plugins=[
             License(; name="MIT"),
+            ProjectFile(; version=v"0.1.0"),
             # Possibly controversially, I think including docs/Manifest.toml is a bad idea
             # - Version constraints should be managed, where necessary, by docs/Project.toml
             # - This avoids commit noise after building documentation locally. 
             Git(;
                 branch="main", ssh=true, ignore=[".DS_Store", "/docs/Manifest.toml"],
             ),
+            Dependabot(),
             Codecov(),
             TagBot(),
             CompatHelper(),
@@ -73,6 +64,7 @@ function tom_template(; dir::Union{Nothing,String}=nothing, julia=v"1.6")
             BlueStyleBadge(),
             ColPracBadge(),
             GitHubActions(),  # CI
+            Formatter(; style="blue"),
             JuliaFormatterGitHubActions(), # FIXME: this is the nasty one.
         ]
     )
