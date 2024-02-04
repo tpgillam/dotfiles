ab4b95c0a6ac8f3251b38a0b09e5c2d78a3ead77
Tom Gillam <tpgillam@googlemail.com>
2024-02-04 11:15:25 +0000

    Async format

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,15 +35,15 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 
 -- Keymappings
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)               -- Show explorer
-vim.keymap.set("n", "<leader>l", vim.cmd.Lazy)              -- Show Lazy manager
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)                                        -- Show explorer
+vim.keymap.set("n", "<leader>l", vim.cmd.Lazy)                                       -- Show Lazy manager
 
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float) -- See more about diagnostics
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)        -- Autoformat with LSP
-vim.keymap.set("n", "gh", vim.lsp.buf.hover)                -- LSP hover action
-vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)   -- LSP code action
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)           -- Go to definition
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation)       -- Go to implementation
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)                          -- See more about diagnostics
+vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end) -- Autoformat with LSP
+vim.keymap.set("n", "gh", vim.lsp.buf.hover)                                         -- LSP hover action
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)                            -- LSP code action
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)                                    -- Go to definition
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)                                -- Go to implementation
 
 -- NOTE: There are other LSP actions that we might want to map, e.g.
 --  - lsp.buf.signature_help
