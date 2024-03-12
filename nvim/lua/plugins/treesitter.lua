cae4b78edfb1d8765a0d24331ec892a37a1504f0
Tom Gillam <tpgillam@googlemail.com>
2024-03-12 09:27:37 +0000

    Format

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function () 
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { 
-				"bash",
-				"c", 
-				"html", 
-				"javascript", 
-				"julia",
-				"lua", 
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "javascript",
+                "julia",
+                "lua",
                 "markdown",
-				"python",
-				"rust",
-				"vim", 
-				"vimdoc", 
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },  
-		})
+                "python",
+                "rust",
+                "vim",
+                "vimdoc",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
 
-		vim.cmd.set("foldmethod=expr")
-		vim.cmd.set("foldexpr=nvim_treesitter#foldexpr()")
-		vim.cmd.set("nofoldenable")
-	end
+        vim.cmd.set("foldmethod=expr")
+        vim.cmd.set("foldexpr=nvim_treesitter#foldexpr()")
+        vim.cmd.set("nofoldenable")
+    end
 }
