d3337a9d844652c82a905c7f9c9ede9bc99522df
Tom Gillam <tpgillam@googlemail.com>
2024-09-07 01:48:11 +0100

    Start unfolded, but with folding enabled

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -26,6 +26,7 @@ return {
 
         vim.cmd.set("foldmethod=expr")
         vim.cmd.set("foldexpr=nvim_treesitter#foldexpr()")
-        vim.cmd.set("nofoldenable")
+        vim.cmd.set("foldenable")
+        vim.cmd.set("foldlevel=99")
     end
 }
